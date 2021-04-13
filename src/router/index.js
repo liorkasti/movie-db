14e3257530dfe06eb66a4052cc79ff3239920b98
Lior Kastenbaum <liorkasti@gmail.com>
2021-04-13 13:36:11 +0300

    Add a delete all logic in favorites list

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,6 @@ import HeaderBar from "../components/HeaderBar";
 import FooterBar from "../components/FooterBar";
 import { TMDB, COLORS } from '../utils/constants';
 import useFetch from '../hooks/useFetch';
-import { appendToFavorites, removeFavorite, updateFavorites } from "../action/modifyActions.js";
 
 const currentUser = auth().currentUser;
 
@@ -29,7 +28,6 @@ export default function Index(props) {
     const [indexPagination, setIndexPagination] = useState(1);
 
     const [initializing, setInitializing] = useState(true);
-    const [loading, setLoading] = useState(true); // Set loading to true on component mount
 
     const [user, setUser] = useState();
     const [users, setUsers] = useState([]);
@@ -40,8 +38,6 @@ export default function Index(props) {
     const [isStared, setIsStared] = useState(false);
 
     useEffect(() => {
-        fetchFavorites()
-        
         console.log("\ncomponentIndex: $0" + componentIndex)
         if (componentIndex === 0) {
             setCurrentComponent("Welcome");
@@ -115,171 +111,178 @@ export default function Index(props) {
         if (popularResult[index].stared) { // remove from favorite list
             popularResult[index].stared = false
             console.log("favoriteList: " + JSON.stringify(favoriteList));
-            console.log("before: " +favoriteList.length)
+            console.log("before: " + favoriteList.length)
             console.log("movie to remove: " + movie.title + " " + movie.id)
             var newList = favoriteList.filter(m => m.id !== movie.id);
             setFavoriteList(newList);
             console.log("after filter: " + favoriteList)
 
-            // database()
-            //     .collection('users')
-            //     .doc(currentUser.email).doc(favorites).update({ favorites: [newList]})
-            //     .then(setFavoriteList(newList))
+            database()
+                .collection('users')
+                .doc(currentUser.email)
+                .update({
+                    favorites: database.FieldValue.delete({movie, stared: true})
+                }).then(setFetchFavorites(favoriteList.filter(m => m.id !== movie.id)))
+
         } else { // add to favorite list
             popularResult[index].stared = true
             database()
                 .collection('users')
-                .doc(currentUser.email).update({ favorites: [...favoriteList, { movie, stared: true }] })
+                .doc(currentUser.email)
+                .update({
+                    favorites: database.FieldValue.arrayUnion({movie, stared: true})
+                })
+                // .update({ favorites: [...favoriteList, { movie, stared: true }] })
                 .then(setFavoriteList([...favoriteList, { movie, stared: true }]))
 
+}
+    }
+
+
+GoogleSignin.configure({
+    webClientId: '1081725631941-b4gol59tl4imc6kj8pf3vc351rhs1cs1.apps.googleusercontent.com',
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+});
+
+const logoff = () => {
+    auth()
+        .signOut()
+        .then(() => console.log('User signed out!'));
+}
+
+onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+}
+
+useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+}, []);
+
+
+if (initializing) return null;
+
+onGoogleButtonPress = async () => {
+    try {
+        const { idToken } = await GoogleSignin.signIn();
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        await GoogleSignin.hasPlayServices();
+        const userInfo = await GoogleSignin.signIn();
+        if (userInfo) {
+            console.log("GOOGLE USER", userInfo.user);
+            await fetchFavorites()
+        }
+        return auth().signInWithCredential(googleCredential);
+    } catch {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        } else {
+            if (error.code === 'auth/email-already-in-use') {
+                console.log('That email address is already in use!');
+            }
+            if (error.code === 'auth/invalid-email') {
+                console.log('That email address is invalid!');
+            }
+            console.error(error);
         }
     }
+}
 
+const Login = () => {
 
-    GoogleSignin.configure({
-        webClientId: '1081725631941-b4gol59tl4imc6kj8pf3vc351rhs1cs1.apps.googleusercontent.com',
-        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    });
-
-    const logoff = () => {
-        auth()
-            .signOut()
-            .then(() => console.log('User signed out!'));
-    }
-
-    onAuthStateChanged = (user) => {
-        setUser(user);
-        if (initializing) setInitializing(false);
-    }
-
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber;
-    }, []);
-
-
-    if (initializing) return null;
-
-    onGoogleButtonPress = async () => {
-        try {
-            const { idToken } = await GoogleSignin.signIn();
-            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-
-            if (userInfo) {
-                console.log("GOOGLE USER", userInfo.user);
-            }
-            return auth().signInWithCredential(googleCredential);
-        } catch {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-            } else {
-                if (error.code === 'auth/email-already-in-use') {
-                    console.log('That email address is already in use!');
-                }
-                if (error.code === 'auth/invalid-email') {
-                    console.log('That email address is invalid!');
-                }
-                console.error(error);
-            }
-        }
-    }
-
-    const Login = () => {
-
-        if (!user) {
-            return (
-                <View>
-                    <GoogleSigninButton
-                        onPress={() => onGoogleButtonPress()}
-                        title="Google Sign-In"
-                        style={styles.btnSocial}
-                        color={GoogleSigninButton.Color.Dark}
-                        size={GoogleSigninButton.Size.Standard}
-                    />
-                </View>
-            );
-        } return (
-            <>
-                {componentIndex === 0 &&
-                    <View style={styles.profileContainer}>
-                        < Image
-                            source={{ uri: user.photoURL }}
-                            style={styles.profileImg}
-                            resizeMode='cover'
-                        />
-                        <Text style={styles.prompt}>Welcome {user.displayName}</Text>
-                        <TouchableOpacity
-                            title="Logout"
-                            onPress={() => logoff()}
-                            style={styles.btnLogout}
-                        >
-                            <Text style={styles.btnTitle}>Logout</Text>
-                        </TouchableOpacity>
-                    </View>
-                }
-            </>
+    if (!user) {
+        return (
+            <View>
+                <GoogleSigninButton
+                    onPress={() => onGoogleButtonPress()}
+                    title="Google Sign-In"
+                    style={styles.btnSocial}
+                    color={GoogleSigninButton.Color.Dark}
+                    size={GoogleSigninButton.Size.Standard}
+                />
+            </View>
         );
-    }
+    } return (
+        <>
+            {componentIndex === 0 &&
+                <View style={styles.profileContainer}>
+                    < Image
+                        source={{ uri: user.photoURL }}
+                        style={styles.profileImg}
+                        resizeMode='cover'
+                    />
+                    <Text style={styles.prompt}>Welcome {user.displayName}</Text>
+                    <TouchableOpacity
+                        title="Logout"
+                        onPress={() => logoff()}
+                        style={styles.btnLogout}
+                    >
+                        <Text style={styles.btnTitle}>Logout</Text>
+                    </TouchableOpacity>
+                </View>
+            }
+        </>
+    );
+}
 
-    const CurrentComponentRouter = () => {
-        if (!components[currentComponent]) return <ActivateIndicator />
-        const CurrentComponent = components[currentComponent];
+const CurrentComponentRouter = () => {
+    if (!components[currentComponent]) return <ActivateIndicator />
+    const CurrentComponent = components[currentComponent];
 
-        return (<CurrentComponent
-            currentComponent={components[componentKeys[componentIndex]]}
+    return (<CurrentComponent
+        currentComponent={components[componentKeys[componentIndex]]}
+        componentIndex={componentIndex}
+
+        onBack={() => { setComponentIndex(componentIndex - 1) }}
+        onNext={() => { setComponentIndex(2) }}
+
+        movieList={movieList}
+        setMovieList={setMovieList}
+
+        popularMovies={popularMovies}
+        popularResult={popularResult}
+        errorFetchMessage={errorFetchMessage}
+
+        favoriteList={favoriteList}
+        setFavoriteList={setFavoriteList}
+
+        indexPagination={indexPagination}
+        setIndexPagination={setIndexPagination}
+
+        renderedMovie={renderedMovie}
+        onSelectedMovie={renderedMovieHandler}
+        setRenderedMovie={setRenderedMovie}
+
+        onPressFavorite={favoritesHandler}
+
+        posterUrlBase={TMDB.posterUrlBase}
+    />)
+};
+
+return (
+    <View style={styles.container}>
+        <HeaderBar
             componentIndex={componentIndex}
 
-            onBack={() => { setComponentIndex(componentIndex - 1) }}
-            onNext={() => { setComponentIndex(2) }}
+            // header={headers[componentKeys[componentIndex]]}
+            onBack={() => {
+                if (componentIndex === 3) setComponentIndex(0);
+                else setComponentIndex(componentIndex - 1);
+            }}
 
-            movieList={movieList}
-            setMovieList={setMovieList}
+            onNext={() => {
+                setComponentIndex(componentIndex + 1)
+            }}
+        />
+        <Login />
+        <CurrentComponentRouter />
 
-            popularMovies={popularMovies}
-            popularResult={popularResult}
-            errorFetchMessage={errorFetchMessage}
-
-            favoriteList={favoriteList}
-            setFavoriteList={setFavoriteList}
-
-            indexPagination={indexPagination}
-            setIndexPagination={setIndexPagination}
-
-            renderedMovie={renderedMovie}
-            onSelectedMovie={renderedMovieHandler}
-            setRenderedMovie={setRenderedMovie}
-
-            onPressFavorite={favoritesHandler}
-
-            posterUrlBase={TMDB.posterUrlBase}
-        />)
-    };
-
-    return (
-        <View style={styles.container}>
-            <HeaderBar
-                componentIndex={componentIndex}
-
-                // header={headers[componentKeys[componentIndex]]}
-                onBack={() => {
-                    if (componentIndex === 3) setComponentIndex(0);
-                    else setComponentIndex(componentIndex - 1);
-                }}
-
-                onNext={() => {
-                    setComponentIndex(componentIndex + 1)
-                }}
-            />
-            <Login />
-            <CurrentComponentRouter />
-
-            <FooterBar handleFooterBar={(screen) => { handleFooterBar(screen) }} />
-        </View >
-    );
+        <FooterBar handleFooterBar={(screen) => { handleFooterBar(screen) }} />
+    </View >
+);
 }
 
 const windowWidth = Dimensions.get('window').width;
